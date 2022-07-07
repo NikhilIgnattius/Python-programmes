87be6bc6030880a81e83f4e85bde8701e6fc98b2
Nikhil Ignatius <89331788+NikhilIgnattius@users.noreply.github.com>
2022-07-07 21:13:41 +0530

    Add files via upload

---
+++ sumOfFirstEvenNumbersUsingRange.py
@@ -0,0 +1,8 @@
+ans = 0
+n = int(input("Enter a number:"))
+
+for i in range(1,n+1):
+    if i % 2 == 0:
+      ans += i 
+ 
+print(ans)
---
+++ swapNumbersWithThirdVariable.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your first number:"))
+b = int(input("Enter your second number:"))
+
+print(a,b)
+
+c = a
+a = b
+b = c
+
+print(a,b)
